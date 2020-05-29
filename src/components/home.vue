c45c0b33cb6554279ba302381823dae440992be4
fuyu <fuyu@123.com>
2020-05-29 16:41:16 +0800

    add

---
+++ src/components/home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="Home">
+    <div>
+    <!-- 标题栏 -->
+    <Mt-header title="提示框">
+      <router-link to="/" slot="left">
+        <!-- <mt-button icon="back">返回</mt-button> -->
+      </router-link>
+    </Mt-header>
+    <!-- 内容 -->
+    <button @click="openTop()">top</button>
+    <button @click="openCenter()">center</button>
+    <button @click="openBottom()">bottom</button>
+    <button @click="openLoading()">loading</button>
+  </div>
+  </div>
+</template>
+
+<script>
+ export default {
+    name: 'Home',
+    data(){
+      return {
+        //
+      }
+    },
+    methods:{
+      openTop(){
+        this.$toast.top('top');
+      },
+      openCenter(){
+        this.$toast.center('center');
+      },
+      openBottom(){
+        this.$toast('bottom');  // or this.$toast.bottom('bottom');
+      },
+      openLoading(){
+        this.$loading('loading...');
+        let self = this;
+        setTimeout(function () {
+          self.closeLoading()
+        }, 2000)
+      },
+      closeLoading(){
+        this.$loading.close();
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  //
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 全局引入Toast
+Vue.use(Toast);
+import './components/Toast/index.css';
+import Toast from './components/Toast/index';
+
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
